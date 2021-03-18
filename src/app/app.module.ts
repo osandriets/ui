316653a3acb73a662e298b7f15b7f594929d65de
Olha Andriiets <o.andriiets@codiv.io>
2021-03-18 16:41:52 +0200

    style settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypographyComponent } from './modules/typography/typography.component';
+import { SharedModule } from './shared/shared.module';
+import { IconsComponent } from './modules/icons/icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent,
+    IconsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
