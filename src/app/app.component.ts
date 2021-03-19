584bb86cc765f6e4cdfb5046e2b8532760fa8ab7
Olha Andriiets <o.andriiets@codiv.io>
2021-03-19 11:33:09 +0200

    add icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui';
+
+  const;
+  logoURL =
+    'https://raw.githubusercontent.com/fireflysemantics/logo/master/l1.svg';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry
+      .addSvgIcon('i-bank',
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/img/icons/bank.svg'))
+      .addSvgIcon('i-calendar',
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/img/icons/calendar.svg'));
+
+  }
+
+  ngOnInit(): void {
+
+    // this.matIconRegistry.addSvgIcon(
+    //   `i-bank`,
+    //   `bank.svg`
+    // );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TypographyComponent } from './modules/typography/typography.component';
 import { SharedModule } from './shared/shared.module';
 import { IconsComponent } from './modules/icons/icons.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TestComponent } from './modules/icons/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
     IconsComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     SharedModule,
   ],
 
